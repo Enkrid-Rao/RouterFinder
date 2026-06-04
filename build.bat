8712f92b9d6e3318d0b8c072f2cb6eb533d157b9
XinxingRao <xinxingrao@gmail.com>
2026-06-04 18:46:36 +0800

    新增build.bat

---
+++ build.bat
@@ -0,0 +1,38 @@
+@echo off
+
+:: 自动寻找 cmake
+for /f "delims=" %%i in ('where cmake 2^>nul') do set "CMAKE=%%~dpi"
+if defined CMAKE goto :found_cmake
+for /d %%d in ("%ProgramFiles%\JetBrains\CLion*") do (
+    if exist "%%d\bin\cmake\win\x64\bin\cmake.exe" (
+        set "CMAKE=%%d\bin\cmake\win\x64\bin"
+        goto :found_cmake
+    )
+)
+for /d %%d in ("%ProgramFiles%\Microsoft Visual Studio\2022\*") do (
+    if exist "%%d\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" (
+        set "CMAKE=%%d\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin"
+        goto :found_cmake
+    )
+)
+:found_cmake
+if defined CMAKE set "PATH=%CMAKE%;%PATH%"
+
+:: 自动寻找 g++
+for /f "delims=" %%i in ('where g++ 2^>nul') do set "MINGW=%%~dpi"
+if defined MINGW goto :found_mingw
+for /d %%d in ("%ProgramFiles%\JetBrains\CLion*") do (
+    if exist "%%d\bin\mingw\bin\g++.exe" (
+        set "MINGW=%%d\bin\mingw\bin"
+        goto :found_mingw
+    )
+)
+:found_mingw
+if defined MINGW set "PATH=%MINGW%;%PATH%"
+
+where cmake >nul 2>&1 || (echo 错误: 未找到 cmake & pause & exit /b 1)
+where g++   >nul 2>&1 || (echo 错误: 未找到 g++   & pause & exit /b 1)
+
+cmake -S . -B build -G "MinGW Makefiles"
+cmake --build build
+echo 编译完成!
--- run.bat
+++ run.bat
@@ -1,8 +1,2 @@
 @echo off
-
-set "PATH=C:\Program Files\JetBrains\CLion 2025.2.5\bin\cmake\win\x64\bin;%PATH%"
-set "PATH=C:\Program Files\JetBrains\CLion 2025.2.5\bin\mingw\bin;%PATH%"
-
-cmake -S . -B build -G "MinGW Makefiles"
-cmake --build build
 build\RouteFinder.exe %*
